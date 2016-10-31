585c727cde44b83e0d07202a262efca1bd5612bd
Alexey1994 <alexeyborisenko20@gmail.com>
2016-10-31 20:12:23 +0200

    sexy commit

--- SQL/init.sql
+++ SQL/init.sql
@@ -31,29 +31,29 @@ CREATE TABLE CarShop.Cars
 	id                  LONG,
 	color_id            LONG,
 	type_id             LONG,
-    model_id            LONG,
-    power               LONG,
-    year_of_manufacture INT,
-    speed               LONG
+	model_id            LONG,
+	power               LONG,
+	year_of_manufacture INT,
+	speed               LONG
 );
 
 
 CREATE TABLE CarShop.Colors
 (
 	id   LONG,
-    name VARCHAR(255)
+	name VARCHAR(255)
 );
 
 
 CREATE TABLE CarShop.CarTypes
 (
 	id   LONG,
-    type VARCHAR(255)
+	type VARCHAR(255)
 );
 
 
 CREATE TABLE CarShop.CarModels
 (
 	id    LONG,
-    model VARCHAR(255)
+	model VARCHAR(255)
 );
