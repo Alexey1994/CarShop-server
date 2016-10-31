49038d7839de680244f96d616380388f3d5b5208
Alexey1994 <alexeyborisenko20@gmail.com>
2016-10-31 17:45:57 +0200

    sexy commit

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -11,9 +11,11 @@ public class Runner extends ServerResource {
     public static void main(String []args) {
         Component   component = new Component();
         Server      server    = component.getServers().add(Protocol.HTTP, 8182);
-        Directory   dir       = new Directory(component.getContext().createChildContext(), "file:///d:/Работа/netcracker/Вёрстка");
+        //Directory   dir       = new Directory(component.getContext().createChildContext(), "file:///d:/Работа/netcracker/Вёрстка");
+        Directory   dir       = new Directory(component.getContext().createChildContext(), "https://alexey1994.github.io/CarShop-static/");
         VirtualHost host      = component.getDefaultHost();
 
+        component.getClients().add(Protocol.HTTPS);
         host.attach(dir);
         Urls.initRoutes(host);
 
